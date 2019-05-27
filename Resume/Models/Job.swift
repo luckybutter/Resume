64a5fe3993e43cbb0713ea6a9ddb1e87860ea167
Matthew Canoy <luckybutter.mattc@gmail.com>
2019-05-27 11:13:34 -0700

    Started App For Resume. This will be fun :)

---
+++ Resume/Models/Job.swift
@@ -0,0 +1,16 @@
+//
+//  Job.swift
+//  Resume
+//
+//  Created by Matthew Canoy on 5/27/19.
+//  Copyright © 2019 luckybutter. All rights reserved.
+//
+
+import Foundation
+
+struct Job: Decodable {
+    let title:String
+    let startDate:Date
+    let endDate:Date
+    
+}
