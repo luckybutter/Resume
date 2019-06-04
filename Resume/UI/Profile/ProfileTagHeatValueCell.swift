13c477fb682c86404d1deb59d814e88bd9690609
Matthew Canoy <luckybutter.mattc@gmail.com>
2019-06-04 02:49:15 -0700

    about to go really. really. fast. maybe?. if xcode-beta doesn't crash on me all night.

---
+++ Resume/UI/Profile/ProfileTagHeatValueCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileTagHeatValueCell.swift
+//  Resume
+//
+//  Created by Matthew Canoy on 5/30/19.
+//  Copyright © 2019 luckybutter. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTagHeatValueCell: UITableViewCell {
+    
+}
